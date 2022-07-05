67b2853037032aa3e9198c0f85e2f677024ee15b
Aninga Abraham <aaninga05@gmail.com>
2022-07-05 22:56:41 +0000

    hot fix and new files

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * jack_bauer - Program that counts down 24 hours like
+ * in the movies
+ *
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+		
+		}
+	}
+}
