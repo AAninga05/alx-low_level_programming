443133a0c7342c2bbf79f182063ff9343174ae78
Aninga Abraham <aaninga05@gmail.com>
2022-07-06 15:59:28 +0000

    hot fixed task 9 & 11 and added task 100, 101 & 103

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * print_times_table - prints n times table, starting with 0
+ * @n: number of times table
+ */
+void print_times_table(int n)
+{
+	int r, c, x;
+i
+	if (x >= 0 && x <= 15)
+	{
+		for (r = 0; r <= n; r++)
+		{
+			for (c = 0; c <= n; c++)
+			{
+				x = r * c;
+				if (r == 0)
+				{
+					_putchar(x + 48);
+				} else if (x < 10 && r != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(x + 48);
+				} else if (c >= 10 && c < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((x / 10) + 48);
+					_putchar((x % 10) + 48);
+				} else if (c >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((x / 100) + 48);
+					_putchar((x / 10) + 48);
+					_putchar((x % 10) + 48);
+				}
+			}
+		_putchar('\n')
+		}
+	}
+}
