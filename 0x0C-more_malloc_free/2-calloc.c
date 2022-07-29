6bcebda9763da9b9af1ec83be8b390479c12a654
Aninga Abraham <aaninga05@gmail.com>
2022-07-29 01:43:58 +0000

    hot fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,14 +8,15 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
-	unsigned int i;
+	unsigned int i, total;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ptr = malloc(nmemb * size);
+	total = nmemb * size;
+	ptr = malloc(total);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < *ptr; i++)
+	for (i = 0; i < total; i++)
 		*(ptr + i) = 0;
 	return (ptr);
 }
