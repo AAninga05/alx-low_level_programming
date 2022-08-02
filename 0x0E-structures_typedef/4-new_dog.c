c43b6f21560eda89f1f7c2e9116d702947c80a25
Aninga Abraham <aaninga05@gmail.com>
2022-08-02 01:59:02 +0000

    adjusted

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,12 @@
 #include "dog.h"
 /**
- *
- *
- *
+ * new_dog - function to create new struct dog_t
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: 0 on success
  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,10 @@
+#include "dog.h"
+/**
+ * free_dog - function that frees dog
+ * @d: pointer to struct dog_t
+ * Return: 0 on success
+ */
+void free_dog(dog_t *d)
+{
+
+}
