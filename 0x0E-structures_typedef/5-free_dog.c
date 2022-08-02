a8760ab2696bbf9d7fd3d532d37ee9e516c35360
Aninga Abraham <aaninga05@gmail.com>
2022-08-02 21:03:56 +0000

    task 4&5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "dog.h"
 /**
  * free_dog - function that frees dog
@@ -7,4 +8,10 @@
 void free_dog(dog_t *d)
 {
 
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
