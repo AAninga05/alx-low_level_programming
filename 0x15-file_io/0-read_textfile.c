b2586e47fcaa0cfd0a0a4b0b5868c59d6cc1872d
Aninga Abraham <aaninga05@gmail.com>
2022-08-22 23:23:08 +0000

    edited task 0-2 for betty errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 /**
  * read_textfile - Reads a text file and prints it to POSIX stdout
  * @filename: Pointer to name of a file
  * @letters: The number of letters that the function should
- * 		read and print
- * Return: if function fails or filename is NULL -> 0
- * 	   O/w - the actual number of bytes the function
- * 	   can print and read
+ *           read and print
+ * Return: If function fails or filename is NULL -> 0
+ *         O/w - the actual number of bytes the function
+ *         can print and read
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -16,10 +16,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buffer = malloc(letters * sizeof(char));
 	if (buffer == NULL)
-		return(0);
+		return (0);
 	if (filename == NULL)
 	{
-		return(0);
+		return (0);
 	}
 
 	o = open(filename, 0_RDONLY);
@@ -29,10 +29,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
-		return(0);
+		return (0);
 	}
 	free(buffer);
 	close(o);
 
-	return(w);
+	return (w);
 }
